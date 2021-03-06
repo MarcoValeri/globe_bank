13211cdff62266d1b95f78b96f5cd0e9e53dd9af
MarcoValeri <marcovaleri@hotmail.it>
2021-03-06 16:46:42 +0000

    Create inser subject function

--- private/query_functions.php
+++ private/query_functions.php
@@ -9,6 +9,47 @@
         return $result;
     }
 
+    // Create a function that find a single user by his/her id
+    function find_subject_by_id($id) {
+        global $db;
+
+        $sql = "SELECT * FROM subjects ";
+        $sql .= "WHERE id='" . $id . "'";
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+        $subject = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+        return $subject; // returns an assoc. array
+    }
+
+    // Create a function that allows to add a subject
+    function insert_subject($menu_name, $position, $visible) {
+        global $db;
+
+        // Handle form values send by new.php
+        $menu_name = $_POST['menu_name'] ?? '';
+        $position = $_POST['position'] ?? '';
+        $visible = $_POST['visible'] ?? '';
+
+        $sql = "INSERT INTO subjects ";
+        $sql .= "(menu_name, position, visible) ";
+        $sql .= "VALUES (";
+        $sql .= "'" . $menu_name . "',";
+        $sql .= "'" . $position . "',";
+        $sql .= "'" . $visible . "'";
+        $sql .= ")";
+        $result = mysqli_query($db, $sql);
+        // For INSERT statements, $result is true/false
+        if ($result) {
+            return true;
+        } else {
+            // INSERT failed
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit;
+        }
+    }
+
     // Create a function that make a query relative psges inside the db
     function find_all_pages() {
         global $db;
--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -2,16 +2,6 @@
     // Require initialize.php and related code
     require_once('../../../private/initialize.php');
 
-    $test = $_GET['test'] ?? '';
-
-    if ($test == '404') {
-        error_404();
-    } elseif ($test == '500') {
-        error_500();
-    } elseif($test == 'redirect') {
-        redirect_to(url_for('/staff/subjects/index.php'));
-    } 
-
 ?>
 
 <?php 
