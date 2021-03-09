7e14266a3af109175298298a93d90e28a181ddb9
MarcoValeri <marcovaleri@hotmail.it>
2021-03-09 17:19:30 +0000

    Add function that allows to edit subjects into the db

--- private/query_functions.php
+++ private/query_functions.php
@@ -50,6 +50,29 @@
         }
     }
 
+    // Create a function that allows to update data into db
+    function update_subject($subject) {
+        global $db;
+
+        $sql = "UPDATE subjects SET ";
+        $sql .= "menu_name='" . $subject['menu_name'] . "', ";
+        $sql .= "position='" . $subject['position'] . "', ";
+        $sql .= "visible='" . $subject['visible'] . "' ";
+        $sql .= "WHERE id='" . $subject['id'] . "' ";
+        $sql .= "LIMIT 1";
+
+        $result = mysqli_query($db, $sql);
+        // For UPDATE statements, $result is true/false
+        if ($result) {
+            return true;
+        } else {
+            // UPDATE failded
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit;
+        }
+    }
+
     // Create a function that make a query relative psges inside the db
     function find_all_pages() {
         global $db;
--- public/staff/subjects/edit.php
+++ public/staff/subjects/edit.php
@@ -7,23 +7,24 @@
     }
 
     $id = $_GET['id'];
-    $menu_name = '';
-    $position = '';
-    $visible = '';
-    
 
     if (is_post_request()) {
 
         // Handle form values send by new.php
-        $menu_name = $_POST['menu_name'] ?? '';
-        $position = $_POST['position'] ?? '';
-        $visible = $_POST['visible'] ?? '';
-    
-        echo "Form parameters<br />";
-        echo "Menu name: " . $menu_name . "<br />";
-        echo "Position: " . $position . "<br />";
-        echo "Visible: " . $visible . "<br />";
-    } 
+        $subject = [];
+        $subject['id'] = $id;
+        $subject['menu_name'] = $_POST['menu_name'] ?? '';
+        $subject['position'] = $_POST['position'] ?? '';
+        $subject['visible'] = $_POST['visible'] ?? '';
+
+        $result = update_subject($subject);
+        redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
+
+    } else {
+
+        $subject = find_subject_by_id($id);
+
+    }
 
 ?>
 
@@ -45,13 +46,13 @@
         <form action="<?= url_for('/staff/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
             <dl>
                 <dt>Menu Name</dt>
-                <dd><input type="text" name="menu_name" value="<?= h($menu_name); ?>" /></dd>
+                <dd><input type="text" name="menu_name" value="<?= h($subject['menu_name']); ?>" /></dd>
             </dl>
             <dl>
                 <dt>Position</dt>
                 <dd>
                     <select name="position">
-                        <option value="1"<?php if ($position == "1") { 
+                        <option value="1"<?php if ($subject['position'] == "1") { 
                             echo " selected";
                         } ?>>1</option>
                     </select>
@@ -61,7 +62,7 @@
                 <dt>Visible</dt>
                 <dd>
                     <input type="hidden" name="visible" value="0" />
-                    <input type="checkbox" name="visible" value="1" <?php if ($visible == "1") {
+                    <input type="checkbox" name="visible" value="1" <?php if ($subject['visible'] == "1") {
                         echo " Checked"; } ?> />
                 </dd>
             </dl>
