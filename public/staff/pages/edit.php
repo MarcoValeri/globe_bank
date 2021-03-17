9e1ca8427ffaa02eedde8898ee1eede618bb0fbd
MarcoValeri <marcovaleri@hotmail.it>
2021-03-17 11:34:19 +0000

    Add functions for validation form

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -1,71 +1,108 @@
 <?php
-    // Require initialize.php and related code
-    require_once('../../../private/initialize.php');
 
-    if (!isset($_GET['id'])) {
-        redirect_to(url_for('/staff/pages/index.php'));
-    }
+require_once('../../../private/initialize.php');
 
-    // Validate form 
-    $id = $_GET['id'];
-    $menu_name = '';
-    $position = '';
-    $visible = '';
+if(!isset($_GET['id'])) {
+  redirect_to(url_for('/staff/pages/index.php'));
+}
+$id = $_GET['id'];
 
-    if (is_post_request()) {
+if(is_post_request()) {
 
-        $menu_name = $_POST['menu_name'] ?? '';
-        $position = $_POST['position'] ?? '';
-        $visible = $_POST['visible'] ?? '';
+  // Handle form values sent by new.php
 
-        echo "Form parameters<br />";
-        echo "Menu name: " . $menu_name . "<br />";
-        echo "Position: " . $position . "<br />";
-        echo "Visible: " . $visible . "<br />";
+  $page = [];
+  $page['id'] = $id;
+  $page['subject_id'] = $_POST['subject_id'] ?? '';
+  $page['menu_name'] = $_POST['menu_name'] ?? '';
+  $page['position'] = $_POST['position'] ?? '';
+  $page['visible'] = $_POST['visible'] ?? '';
+  $page['content'] = $_POST['content'] ?? '';
 
-    }
+  $result = update_page($page);
+  redirect_to(url_for('/staff/pages/show.php?id=' . $id));
 
-    // Created a variable that stores the title of the page
-    $page_title = 'Edit Page';
+} else {
 
-    // Include staff_header.php and related code
-    include(SHARED_PATH . '/staff_header.php');
+  $page = find_page_by_id($id);
+
+  $page_set = find_all_pages();
+  $page_count = mysqli_num_rows($page_set);
+  mysqli_free_result($page_set);
+
+}
 
 ?>
 
+<?php $page_title = 'Edit Page'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
 <div id="content">
-    <a class="back-link" href="<?= url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
 
-    <div class="subject new">
-        <h1>Edit Page</h1>
+  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
 
-        <form action="<?= url_for('/staff/pages/new.php?id=' . h(u($id))); ?>" method="post">
-            <dl>
-                <dt>Menu Name</dt>
-                <dd><input type="text" name="menu_name" value="<?= h($menu_name); ?>" /></dd>
-            </dl>
-            <dl>
-                <dt>Position</dt>
-                <dd>
-                    <select name="position">
-                        <option value="1"<?php if($position == "1") { echo " selected";
-                        } ?>>1</option>
-                    </select>
-                </dd>
-            </dl>
-            <dl>
-                <dt>Visible</dt>
-                <dd>
-                    <input type="hidden" name="visible" value="0" />
-                    <input type="checkbox" name="visible" value="1"<?php if($visible == 1) {
-                        echo " checked";
-                    }
-                    ?> />
-                </dd>
-            </dl>
-            <div id="operations">
-                <input type="submit" value="Create Page" />
-            </div>
-        </form>
-    </div>
+  <div class="page edit">
+    <h1>Edit Page</h1>
+
+    <form action="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($id))); ?>" method="post">
+      <dl>
+        <dt>Subject</dt>
+        <dd>
+          <select name="subject_id">
+          <?php
+            $subject_set = find_all_subjects();
+            while($subject = mysqli_fetch_assoc($subject_set)) {
+              echo "<option value=\"" . h($subject['id']) . "\"";
+              if($page["subject_id"] == $subject['id']) {
+                echo " selected";
+              }
+              echo ">" . h($subject['menu_name']) . "</option>";
+            }
+            mysqli_free_result($subject_set);
+          ?>
+          </select>
+        </dd>
+      </dl>
+      <dl>
+        <dt>Menu Name</dt>
+        <dd><input type="text" name="menu_name" value="<?php echo h($page['menu_name']); ?>" /></dd>
+      </dl>
+      <dl>
+        <dt>Position</dt>
+        <dd>
+          <select name="position">
+            <?php
+              for($i=1; $i <= $page_count; $i++) {
+                echo "<option value=\"{$i}\"";
+                if($page["position"] == $i) {
+                  echo " selected";
+                }
+                echo ">{$i}</option>";
+              }
+            ?>
+          </select>
+        </dd>
+      </dl>
+      <dl>
+        <dt>Visible</dt>
+        <dd>
+          <input type="hidden" name="visible" value="0" />
+          <input type="checkbox" name="visible" value="1"<?php if($page['subject_id'] == "1") { echo " checked"; } ?> />
+        </dd>
+      </dl>
+      <dl>
+        <dt>Content</dt>
+        <dd>
+          <textarea name="content" cols="60" rows="10"><?php echo h($page['content']); ?></textarea>
+        </dd>
+      </dl>
+      <div id="operations">
+        <input type="submit" value="Edit Page" />
+      </div>
+    </form>
+
+  </div>
+
 </div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
--- public/staff/pages/index.php
+++ public/staff/pages/index.php
@@ -1,63 +1,53 @@
-<?php
-    // Require initialize.php and related code
-    require_once('../../../private/initialize.php');
-?>
+<?php require_once('../../../private/initialize.php'); ?>
 
 <?php
-    // Save all page by the db inside a variable
-    $page_set = find_all_pages();
+
+  $page_set = find_all_pages();
 
 ?>
 
-<?php
-    // Set the page title inside a variable
-    $page_title = "Pages";
-?>
-
-<?php
-    // Include staff_header.php and related code
-    include(SHARED_PATH . '/staff_header.php');
-?>
+<?php $page_title = 'Pages'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
-    <div class="pages listing">
-        <h1>Pages</h1>
+  <div class="pages listing">
+    <h1>Pages</h1>
 
-        <div class="actions">
-            <a class="action" href="<?= url_for('staff/pages/new.php'); ?>">Create New Page</a>
-        </div>
-
-        <table class="list">
-            <tr>
-                <th>ID</th>
-                <th>Position</th>
-                <th>Visible</th>
-                <th>Name</th>
-                <th>&nbsp;</th>
-                <th>&nbsp;</th>
-                <th>&nbsp;</th>
-            </tr>
-
-            <?php while($page = mysqli_fetch_assoc($page_set)) { ?>
-            <tr>
-                <td><?= h($page['id']); ?></td>
-                <td><?= h($page['subject_id']); ?></td>
-                <td><?= h($page['position']); ?></td>
-                <td><?= $page['visible'] == 1 ? 'true' : 'false'; ?></td>
-                <td><?= h($page['menu_name']); ?></td>
-                <td><a class="action" href="<?= url_for('staff/pages/show.php?id=' . h(u($page['id']))); ?>">View</a></td>
-                <td><a class="action" href="<?= url_for('staff/pages/edit.php?id=' . h(u($page['id']))); ?>">Edit</a></td>
-                <td><a class="action" href="">Delete</a></td>
-            </tr>
-            <?php } ?>
-        </table>
-
-        <?php mysqli_free_result($page_set); ?>
-
+    <div class="actions">
+      <a class="action" href="<?php echo url_for('/staff/pages/new.php'); ?>">Create New Page</a>
     </div>
+
+  	<table class="list">
+  	  <tr>
+        <th>ID</th>
+        <th>Subject</th>
+        <th>Position</th>
+        <th>Visible</th>
+  	    <th>Name</th>
+  	    <th>&nbsp;</th>
+  	    <th>&nbsp;</th>
+        <th>&nbsp;</th>
+  	  </tr>
+
+      <?php while($page = mysqli_fetch_assoc($page_set)) { ?>
+        <?php $subject = find_subject_by_id($page['subject_id']); ?>
+        <tr>
+          <td><?php echo h($page['id']); ?></td>
+          <td><?php echo h($subject['menu_name']); ?></td>
+          <td><?php echo h($page['position']); ?></td>
+          <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
+    	    <td><?php echo h($page['menu_name']); ?></td>
+          <td><a class="action" href="<?php echo url_for('/staff/pages/show.php?id=' . h(u($page['id']))); ?>">View</a></td>
+          <td><a class="action" href="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($page['id']))); ?>">Edit</a></td>
+          <td><a class="action" href="<?php echo url_for('/staff/pages/delete.php?id=' . h(u($page['id']))); ?>">Delete</a></td>
+    	  </tr>
+      <?php } ?>
+  	</table>
+
+    <?php mysqli_free_result($page_set); ?>
+
+  </div>
+
 </div>
 
-<?php
-    // Include staff_footer.php and related code
-    include(SHARED_PATH . '/staff_footer.php');
-?>
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
