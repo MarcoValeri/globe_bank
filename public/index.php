0a810d15c4fc1f9947bc14888ddd1161b7de832f
Marco Valeri <marcovaleri@hotmail.it>
2021-05-05 08:52:56 +0200

    Add static home page

--- public/index.php
+++ public/index.php
@@ -8,6 +8,15 @@
 
   <div id="page">
 
+    <?php 
+      // Show the homepage
+      // The homepage content could:
+      // * the static content (here or in the shared file)
+      // * show the first page the nav
+      // * be in the database but add code to be hide in th nav
+      include(SHARED_PATH . '/static_homepage.php'); 
+    ?>
+
   </div>
 
 </div>
