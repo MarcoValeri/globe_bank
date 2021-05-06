821db7191ccf17e06511a98732724997d36971a6
Marco Valeri <marcovaleri@hotmail.it>
2021-05-06 09:31:49 +0200

    Add interaction between navbar and database

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php require_once('../private/initialize.php'); ?>
 
+<?php
+  if (isset($_GET['id'])) {
+    $page_id = $_GET['id'];
+    $page = find_page_by_id($page_id);
+    if (!$page) {
+      redirect_to(url_for('/index.php'));
+    }
+  } else {
+    // nothing selected; show the homepage
+  }
+?>
+
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="main">
@@ -9,12 +21,18 @@
   <div id="page">
 
     <?php 
-      // Show the homepage
-      // The homepage content could:
-      // * the static content (here or in the shared file)
-      // * show the first page the nav
-      // * be in the database but add code to be hide in th nav
-      include(SHARED_PATH . '/static_homepage.php'); 
+      if (isset($page)) {
+        // show the page from database
+        // TODO add html escaping back
+        echo $page['content'];
+      } else {
+        // Show the homepage
+        // The homepage content could:
+        // * the static content (here or in the shared file)
+        // * show the first page the nav
+        // * be in the database but add code to be hide in th nav
+        include(SHARED_PATH . '/static_homepage.php'); 
+      }
     ?>
 
   </div>
