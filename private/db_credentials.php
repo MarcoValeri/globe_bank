cf6a2ef42044e719a4117c752acdf8e769ec629a
Marco Valeri <marcovaleri@hotmail.it>
2021-05-27 14:32:57 +0100

    Add function that allow to hidden the content that has visibility == false

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,13 @@
 <?php
 
+    // define("DB_SERVER", "localhost");
+    // define("DB_USER", "marco_globe_bank");
+    // define("DB_PASS", "Silver09");
+    // define("DB_NAME", "globe_bank");
+
     define("DB_SERVER", "localhost");
-    define("DB_USER", "marco_globe_bank");
-    define("DB_PASS", "Silver09");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
     define("DB_NAME", "globe_bank");
 
 ?>
